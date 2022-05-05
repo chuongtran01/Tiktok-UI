20b1366ab850f29252d550286eb7a76c0d8ca6f0
Chuong Tran <chuongtran2001@gmail.com>
2022-05-04 22:11:10 -0500

    62

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            // const Layout = route.layout === HeaderOnly ? HeaderOnly : DefaultLayout
+
+            let Layout = DefaultLayout
+
+            if (route.layout) {
+              Layout = route.layout
+            }
+            else if (route.layout === null) {
+              Layout = Fragment
+            }
+
+            // element contains component so we need to have the line below
+            const Page = route.component
+            return <Route key={index} path={route.path} element={<Layout><Page/></Layout>}/>
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
